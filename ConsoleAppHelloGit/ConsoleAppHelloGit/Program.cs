885be0b0de5d2b153bccaac21e2fabbf0d1142f8
Esmaeil Mortezazadeh <Esi.cloud@outlook.com>
2020-05-26 10:10:46 +0430

    hh2

--- ConsoleAppHelloGit/ConsoleAppHelloGit/Program.cs
+++ ConsoleAppHelloGit/ConsoleAppHelloGit/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppHelloGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git!");
+            Console.WriteLine("Hello Git!2");
         }
     }
 }
